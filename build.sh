6b740dfec89a826d118890594a3bcfe9192bd9bd
Tincho <salasmartin0501@gmail.com>
2025-11-25 01:06:54 -0300

    config: agrego un script para debuggear vercell

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "--- INICIANDO INSTALACION .NET ---"
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel 8.0 --install-dir .dotnet
+export DOTNET_ROOT=$(pwd)/.dotnet
+export PATH=$PATH:$DOTNET_ROOT
+
+echo "--- PUBLICANDO PROYECTO ---"
+dotnet publish Portfolio.Client/Portfolio.Client.csproj -c Release -o output
+
+echo "--- BUSCANDO EL ARCHIVO PERDIDO ---"
+# Esto nos dirá en los logs dónde está exactamente el archivo
+find output -name "blazor.webassembly.js"
